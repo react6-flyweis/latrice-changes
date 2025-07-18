949437c3a7debc34a4eb077342188bd9dc3dc13e
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-19 00:56:44 +0530

    feat(independent dash): enhance component with ConfirmModal for payment and connection requests

--- src/pages/Independent Dashboard/Family & Friends/FamilyCards.jsx
+++ src/pages/Independent Dashboard/Family & Friends/FamilyCards.jsx
@@ -1,58 +1,134 @@
-import React from 'react';
-import img from '../../../assets/images/dashboard/img36.jpg'
-import { RiDeleteBin6Fill } from "react-icons/ri";
-import { BiSolidEditAlt } from "react-icons/bi";
+import img from "../../../assets/images/dashboard/img36.jpg";
+import { ConfirmModal } from "../../../components/Modals/ConfirmModal";
 
-const FamilyCards = ({setModalOpen}) => {
-    return (
-        <div className="bg-white rounded-[20px] shadow-[0px_4px_4px_0px_#00000040] ">
-            {/* Top section */}
-            <div className="flex items-start gap-3 p-2">
-                <img
-                    src={img}
-                    alt="Profile"
-                    className="w-[100px] rounded-[10px] object-cover"
-                />
-                <div className="w-[350px]">
-                    <p className='flex items-center justify-between font-sansation font-[700] sm:text-[19px] text-[15px] text-charcoal '>
-                        <span className="">Relation:</span>
-                        Friend
-                    </p>
-                    <p className='flex items-center justify-between font-sansation font-[700] sm:text-[20px] text-[15px] text-charcoal '>
-                        <span className="">Name:</span>
-                        Jordyn Mango
-                    </p>
-                    <p className='flex items-center justify-between font-sansation font-[700] sm:text-[20px] text-[15px] text-charcoal '>
-                        <span className="">Phone Number:</span>
-                        +1 778 6931 369
-                    </p>
-                    <p className='flex items-center justify-between font-sansation font-[700] sm:text-[20px] text-[15px] text-charcoal '>
-                        <span className="">Email Address:</span>
-                        example@gmail.com
-                    </p>
-                    <p className='flex items-center justify-between font-sansation font-[700] sm:text-[20px] text-[15px] text-charcoal '>
-                        <span className="">Birthday:</span>
-                        1996/07/07
-                    </p>
-                </div>
+const FamilyCards = ({
+  data,
+  isPending,
+  showmodal,
+  showmodal1,
+  showmodal2,
+  showmodal4,
+  showmodal5,
+  showmodal6,
+}) => {
+  return (
+    <div className="bg-white border border-[#2F2F2F80] rounded-[20px] shadow-[0px_4px_4px_0px_#00000040] px-3 py-2.5">
+      <div className="flex flex-col gap-2">
+        <div className="flex items-start justify-between gap-3">
+          <div className="flex items-start gap-3">
+            <img
+              src={img}
+              alt="Profile"
+              className="w-[94px] h-[94px] rounded-full object-cover"
+            />
+            <div>
+              <h6 className="font-[700] text-[18px] sm:text-[22px] text-charcoal font-sansation">
+                {data.name}
+              </h6>
+              <p className="font-[400] text-[15px] sm:text-[18px] text-charcoal font-sansation flex items-center gap-2">
+                {data.service && !isPending && (
+                  <p className="font-[400] text-[15px] sm:text-[18px] text-charcoal font-sansation flex gap-3">
+                    <span>{data.service}</span> ·{" "}
+                    <span>{data.paymentStatus}</span>
+                  </p>
+                )}
+                {data.status}
+              </p>
             </div>
+          </div>
 
-            {/* Buttons */}
-            <div className="flex items-center mt-2">
-                <button className="w-full bg-[#97E7EC] text-[#123E41] font-semibold sm:text-[20px] text-lg px-2 py-2 rounded-bl-[20px] shadow-[0px_4px_4px_0px_#00000040] hover:bg-[#6cc8cf] transition flex gap-1 items-center justify-center"
-                >
-                    <RiDeleteBin6Fill size={20} />
-                    Delete
-                </button>
-                <button className="w-full bg-[#FFE6D8] text-[#FF827F] font-semibold sm:text-[20px] text-lg px-2 py-2  rounded-br-[20px] shadow-[0px_4px_4px_0px_#00000040] hover:bg-[#fbbcb5] transition flex gap-1 items-center justify-center"
-                    onClick={() => setModalOpen(true)}
-                >
-                    <BiSolidEditAlt size={20} />
-                    Edit
-                </button>
+          {data.timeRemaining ? (
+            <div className="font-[700] text-[18px] sm:text-[22px] text-charcoal font-sansation">
+              {data.timeRemaining}
             </div>
+          ) : isPending ? (
+            <div className="flex gap-2">
+              <div
+                onClick={showmodal5}
+                className=" cursor-pointer border border-[#2F2F2F] px-2 py-1 rounded-[4px] text-[15px] sm:text-[18px] text-charcoal"
+              >
+                Decline
+              </div>
+              <div
+                onClick={showmodal6}
+                className="cursor-pointer  border border-[#2F2F2F] px-2 py-1 rounded-[4px] text-[15px] sm:text-[18px] text-charcoal"
+              >
+                Accept
+              </div>
+            </div>
+          ) : (
+            <div
+              onClick={showmodal1}
+              className="border border-[#2F2F2F] px-2 py-1 rounded-[4px] text-[15px] sm:text-[18px] text-charcoal cursor-pointer"
+            >
+              Deactivate member
+            </div>
+          )}
         </div>
-    );
+
+        <div className="flex justify-end items-center gap-2">
+          {data.status == "remaining" && (
+            <div
+              onClick={showmodal1}
+              className="border border-[#2F2F2F] px-2 py-1 rounded-[4px] text-[15px] sm:text-[18px] text-charcoal cursor-pointer"
+            >
+              Deactivate member
+            </div>
+          )}
+          <div
+            onClick={showmodal}
+            className=" cursor-pointer border border-[#2F2F2F] px-2 py-1 rounded-[4px] text-[15px] sm:text-[18px] text-charcoal"
+          >
+            See Details
+          </div>
+          {data.timeRemaining && isPending && (
+            <>
+              <ConfirmModal
+                text={`Confirm Payment & Connection?
+You’re about to pay $85 for Nicole White’s appointment and add her to your Family & Friends list.   To review the service details you are about to pay for, tap “See Details” in the previous screen before confirming.`}
+                onConfirm={() => {}}
+                confirmText="Confirm & Pay"
+              >
+                <button className="cursor-pointer border! border-[#2F2F2F] px-2 py-1 rounded-[4px] text-[15px] sm:text-[18px] text-charcoal">
+                  Decline & Reject Payment
+                </button>
+              </ConfirmModal>
+              <ConfirmModal
+                text={`Decline Connection & Payment Request?
+You’re about to decline Nicole White’s request to connect and reject the payment request for her appointment.She will be notified and removed from your pending Family & Friends list.`}
+                confirmText="Yes, Decline"
+                onConfirm={() => {}}
+              >
+                <button className="cursor-pointer border! border-[#2F2F2F] px-2 py-1 rounded-[4px] text-[15px] sm:text-[18px] text-charcoal">
+                  Accept & Pay Now
+                </button>
+              </ConfirmModal>
+            </>
+          )}
+        </div>
+        <div className="flex justify-end items-center gap-2 mt-2">
+          {data.btn1 && (
+            <div
+              onClick={showmodal2}
+              className="border border-[#2F2F2F] px-2 py-1 rounded-[4px] text-[15px] sm:text-[18px] text-charcoal cursor-pointer"
+            >
+              {data.btn1}
+            </div>
+          )}
+          {data.btn2 && (
+            <div
+              onClick={
+                data.btn2 === "Decline Request" ? showmodal5 : showmodal4
+              }
+              className="border border-[#2F2F2F] px-2 py-1 rounded-[4px] text-[15px] sm:text-[18px] text-charcoal cursor-pointer"
+            >
+              {data.btn2}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FamilyCards;
